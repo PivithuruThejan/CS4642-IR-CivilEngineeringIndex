1759980a0df36609b69645a0bc0234ea02e41cbb
PivithuruThejan <pivithuru.14@cse.mrt.ac.lk>
2018-06-16 23:37:37 +0530

    Collected data in links

--- teaIndex/teaIndex/spiders/constructor_spider.py
+++ teaIndex/teaIndex/spiders/constructor_spider.py
@@ -13,10 +13,24 @@ class QuotesSpider(scrapy.Spider):
 
     def parse(self, response):
         for article in  response.css('div.post-inner' ):
-            yield {
-                'title': article.css('div.post-inner ::text').extract()[1],
-                'text':  article.css('div.post-inner ::text').extract()[8],
-                'link': article.css('div.post-content a::attr(href)').extract()[0],
-            }
+            link = article.css('div.post-content a::attr(href)').extract()[0]
+            #yield {
+                #'title': article.css('div.post-inner ::text').extract()[1],
+                #'text':  article.css('div.post-inner ::text').extract()[8],
+                #'link': article.css('div.post-content a::attr(href)').extract()[0],
+            #}
+
+            yield scrapy.Request(url = link, callback =self.parse2)
 
 #follow pagination link
+    def parse2(self, response):
+        title = response.css('div.post-inner')
+        for line in response.css('div.post-content'):
+            yield {
+                'title':title.css('span.entry-title ::text').extract(),
+                'headings': line.css('h2 ::text').extract(),
+                'sub headings': line.css('h3 ::text').extract(),
+                'text': line.css('p ::text').extract(),
+
+
+            }
